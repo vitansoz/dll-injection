11a245ef1a8365bb4c8fcfb03800642b32078534
elegnate <37270569+elegnate@users.noreply.github.com>
2018-08-09 00:10:22 +0900

    Create src.cpp

---
+++ src.cpp
@@ -0,0 +1,112 @@
+BOOL CUSBwApp::InjectDll
+(
+	DWORD dwPID,
+	LPCTSTR szDllPath
+)
+{
+	HANDLE                  hProcess, hThread;
+	LPVOID                  pRemoteBuf;
+	DWORD                   dwBufSize = (DWORD)(_tcslen(szDllPath) + 1) * sizeof(TCHAR);
+	LPTHREAD_START_ROUTINE  pThreadProc;
+
+
+	if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID))) {
+		return FALSE;
+	}
+
+	pRemoteBuf = VirtualAllocEx(hProcess, NULL, dwBufSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	WriteProcessMemory(hProcess, pRemoteBuf, (LPVOID)szDllPath, dwBufSize, NULL);
+	pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryW");
+	hThread = CreateRemoteThread(hProcess, NULL, 0, pThreadProc, pRemoteBuf, 0, NULL);
+	WaitForSingleObject(hThread, INFINITE);
+
+	VirtualFreeEx(hProcess, pRemoteBuf, 0, MEM_RELEASE);
+
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+
+	return TRUE;
+}
+
+
+BOOL CUSBwApp::EjectDll
+(
+	DWORD dwPID,
+	LPCTSTR szDllPath
+)
+{
+	BOOL                    bMore = FALSE, bFound = FALSE;
+	HANDLE                  hSnapshot, hProcess, hThread;
+	MODULEENTRY32           me = { sizeof(me) };
+	LPTHREAD_START_ROUTINE  pThreadProc;
+
+	if (INVALID_HANDLE_VALUE ==
+		(hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID))) {
+		return FALSE;
+	}
+
+	bMore = Module32First(hSnapshot, &me);
+
+	do {
+		if ((_tcsicmp(me.szModule,  szDllPath) == NO_ERROR) ||
+			(_tcsicmp(me.szExePath, szDllPath) == NO_ERROR)) {
+			bFound = TRUE;
+			break;
+		}
+	} while (bMore = Module32Next(hSnapshot, &me));
+
+	if (!bFound) {
+		CloseHandle(hSnapshot);
+		return FALSE;
+	}
+
+	if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID))) {
+		CloseHandle(hSnapshot);
+		return FALSE;
+	}
+
+	pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "FreeLibrary");
+	hThread = CreateRemoteThread(hProcess, NULL, 0, pThreadProc, me.modBaseAddr, 0, NULL);
+	WaitForSingleObject(hThread, INFINITE);
+
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+	CloseHandle(hSnapshot);
+
+	return TRUE;
+}
+
+
+BOOL CUSBwApp::InjectTargetProcess
+(
+  LPCTSTR szDllPath,
+  BOOL bInject
+)
+{
+	HANDLE         hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	PROCESSENTRY32 entry32;
+	BOOL           bResult = FALSE;
+
+
+	entry32.dwSize = sizeof(PROCESSENTRY32);
+
+	if (hSnapshot == INVALID_HANDLE_VALUE) {
+		return bResult;
+	}
+
+	BOOL bRet = Process32First(hSnapshot, &entry32);
+
+	do {
+		if (_tcsicmp(entry32.szExeFile, TARGET_PROC_NAME) == 0) {
+			if (bInject) {
+				InjectDll(entry32.th32ProcessID, szDllPath);
+			} else {
+				EjectDll(entry32.th32ProcessID, szDllPath);
+			}
+			break;
+		}
+	} while (bRet = Process32Next(hSnapshot, &entry32));
+
+	CloseHandle(hSnapshot);
+	return bResult;
+}
